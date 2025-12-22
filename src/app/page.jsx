a7bcd8639923e5c8f47ea5acca7e23443c7008e3
SowvikSuvo <skarmoker44@gmail.com>
2025-12-22 22:59:31 +0600

    setup next js and install package

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" flex items-center justify-center font-bold text-5xl text-purple-500">
+      Homepage
+    </div>
+  );
+}
